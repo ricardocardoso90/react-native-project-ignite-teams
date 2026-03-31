e9d881a9155923601981dfc2b96993b33d6143bc
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-03-31 11:36:11 -0300

    update commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import theme from '@theme/theme';
+import { Routes } from './src/routes';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import theme from '@theme/theme';
 import { Loading } from '@components/Loading';
-import { Routes } from './src/routes';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,4 +22,4 @@ export default function App() {
       }
     </ThemeProvider>
   );
-}
+};
